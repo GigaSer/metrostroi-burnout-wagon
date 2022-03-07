6a7f76a6c1a260f1a4c3c48d9297522944fd86e3
GigaSer <sergvas1508@yandex.ru>
2022-03-08 00:42:17 +0400

    Get random wagon in train

--- lua/firewagon/cl_firewagon.lua
+++ lua/firewagon/cl_firewagon.lua
@@ -1,1 +1,8 @@
+local function cmdHandler()
+    net.Start("FuckMyRandomWagonByFire")
+    net.SendToServer()
+end
+
+concommand.Add("ignitemywagon", cmdHandler, nil, "Burn out your random wagon in train you seat", FCVAR_CHEAT)
+
 print("client firewagon enable")
--- lua/firewagon/sv_firewagon.lua
+++ lua/firewagon/sv_firewagon.lua
@@ -1,1 +1,18 @@
+util.AddNetworkString("FuckMyRandomWagonByFire")
+
+local function igniteRequestReceiver(contentLength, ply)
+    local poorTrain = ply:GetTrain()
+    if poorTrain == nil then
+        ply:ChatPrint("Поджигать нечего")
+        return
+    end
+    poorTrain:UpdateWagonList()
+    local randomWagon = poorTrain.WagonList[math.random(0, poorTrain:GetWagonCount())]
+    
+    print(randomWagon:GetWagonNumber())
+    sound.Play( "phx/explode00.wav", randomWagon:GetPos(), 180, 100, 1)
+end
+
+net.Receive("FuckMyRandomWagonByFire", igniteRequestReceiver)
+
 print("server firewagon enable")
